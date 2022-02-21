6d6f12026dd442686a6828b117e4b669d7fb31a7
Simone Mauro <simon.mauro@virgilio.it>
2022-02-21 15:42:15 +0100

    Header completato

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <myHeader/>
+  </div>
+</template>
+
+<script>
+import myHeader from './components/myHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    myHeader
+  }
+}
+</script>
+
+<style lang="scss">
+
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: 'Roboto', sans-serif;
+  }
+  
+</style>
---
+++ src/components/myHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+      <img src="../assets/img/dc-logo.png" alt="dc-logo">
+
+      <nav>
+          <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>Tv</li>
+              <li>Games</li>
+              <li>Collectibles</li>
+              <li>Videos</li>
+              <li>Fans</li>
+              <li>News</li>
+              <li>Shop</li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'myHeader',
+  
+}
+
+</script>
+
+<style lang="scss">
+    header{
+        width: 80%;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 30px;
+
+        img {
+            margin-right: 250px;
+        }
+
+        ul{
+            display: flex;
+        }
+
+        li{
+            padding: 15px;
+            display: flex;
+            align-items: center;
+            border-bottom: 2px solid transparent;
+
+            &:hover{
+                color: #0282f9;
+                border-bottom: 2px solid #0282f9;
+                cursor: pointer;
+
+            }
+        }
+    }
+
+</style>
